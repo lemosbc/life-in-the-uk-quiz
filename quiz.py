61fa08e78557045d63bf1d0a3e6d8815d76c0fff
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2022-12-08 21:54:54 +0000

    Change QUESTIONS form a list of tuples to a dictionary to store alternatives

--- quiz.py
+++ quiz.py
@@ -1,21 +1,28 @@
 #quiz.py
 
-QUESTIONS = [
-    ("Which king, who succeeded Henry VIII, died when he was 15 years old", 
-    "Edward VI" ),
-    ("Is the statement below TRUE or FALSE? Charles II marched into England with a Scottish army to reclaim his throne." ,
-    "True"),
-    ("Which country of the UK is not represented on the Union Flag",
-    "Wales"),
-    ("Which of these people was a great British playwright", 
-    "William Shakespeare"),
-    ("When were the first professional football clubs formed", 
-    "19th Century")
-]
+QUESTIONS = {
+    "Which king, who succeeded Henry VIII, died when he was 15 years old" : [
+        "Edward VI", "Charles I", "Harold", "Richard III"
+    ], 
+    "Is the statement below TRUE or FALSE? Charles II marched into England with a Scottish army to reclaim his throne." : [
+        "True", "False"
+    ],
+    "Which country of the UK is not represented on the Union Flag" : [
+        "Wales", "Scotland", "Northern Ireland", "England"
+    ],
+    "Which of these people was a great British playwright" : [
+        "William Shakespeare", "Sir Francis Drake", "Geoffrey Chaucer", "William Caxton"
+    ],
+    "When were the first professional football clubs formed" : [
+        "19th Century", "The Middle Ages", "17th Century", "20th Century"
+    ]
+}
 
-for question, correct_answer in QUESTIONS:
-    answer = input(f"{question}? ")
-    if answer == correct_answer:
-        print("Correct!")
-    else:
-        print(f"The answer is {correct_answer!r}, not {answer!r}")
+print(QUESTIONS)
+
+#for question, correct_answer in QUESTIONS:
+#    answer = input(f"{question}? ")
+#    if answer == correct_answer:
+#        print("Correct!")
+#    else:
+#        print(f"The answer is {correct_answer!r}, not {answer!r}")
