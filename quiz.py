4bcf534a981a006d734e260b38c9312a4461381a
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2022-12-08 21:39:18 +0000

    Set for loop for gathering input and comparing with correct answer

--- quiz.py
+++ quiz.py
@@ -1,16 +1,21 @@
 #quiz.py
 
 QUESTIONS = [
-    ("Which king, who succeeded Henry VIII, died when he was 15 years old?", 
+    ("Which king, who succeeded Henry VIII, died when he was 15 years old", 
     "Edward VI" ),
-    ("Is the statement below TRUE or FALSE? Charles II marched into England with a Scottish army to reclaim his throne. " ,
+    ("Is the statement below TRUE or FALSE? Charles II marched into England with a Scottish army to reclaim his throne." ,
     "True"),
-    ("Which country of the UK is not represented on the Union Flag? ",
+    ("Which country of the UK is not represented on the Union Flag",
     "Wales"),
-    ("Which of these people was a great British playwright? ", 
+    ("Which of these people was a great British playwright", 
     "William Shakespeare"),
-    ("When were the first professional football clubs formed? ", 
+    ("When were the first professional football clubs formed", 
     "19th Century")
 ]
 
-print(QUESTIONS)
+for question, correct_answer in QUESTIONS:
+    answer = input(f"{question}? ")
+    if answer == correct_answer:
+        print("Correct!")
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
