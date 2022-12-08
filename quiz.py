afa23a21ca29fc906980770e7195a4dedf9bcaaf
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2022-12-08 21:32:35 +0000

    Set QUESTION variable as a list of 5 questions and answers tuples

---
+++ quiz.py
@@ -0,0 +1,16 @@
+#quiz.py
+
+QUESTIONS = [
+    ("Which king, who succeeded Henry VIII, died when he was 15 years old?", 
+    "Edward VI" ),
+    ("Is the statement below TRUE or FALSE? Charles II marched into England with a Scottish army to reclaim his throne. " ,
+    "True"),
+    ("Which country of the UK is not represented on the Union Flag? ",
+    "Wales"),
+    ("Which of these people was a great British playwright? ", 
+    "William Shakespeare"),
+    ("When were the first professional football clubs formed? ", 
+    "19th Century")
+]
+
+print(QUESTIONS)
