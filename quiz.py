502bc33beb472f22e4f465ec9b03d68e885018b0
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2022-12-14 23:47:25 +0000

    Add functionality for multiple correct answers
    Changes made in the functions get_answers, ask_question and in
    questions.toml to allow for questions with multiple correct answers.

--- quiz.py
+++ quiz.py
@@ -9,7 +9,7 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
-NUM_QUESTIONS_PER_QUIZ = 5
+NUM_QUESTIONS_PER_QUIZ = 6
 #uses pathlib to tell script that questions.toml is in the same folder
 #as quiz.py
 QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
@@ -30,36 +30,59 @@ def prepare_questions(path, num_questions):
 #interacts with user display a question, labeling alternatives with a lowercase
 #letter and uses zip to pair label and alternatives into a dictionary
 #returns labeled alternatives
-def get_answer(question, alternatives):
+def get_answers(question, alternatives, num_choices = 1):
     print(f"{question}?")
     #string.ascii_lowercase uses letters to label alternatives
     labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
     for label, alternative in labeled_alternatives.items():
         print(f" {label}) {alternative}")
-    #determines rather user input is a valid label and if not prompts user 
-    #for a valid input
-    #assigns user input to answer_label
-    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
-        print(f"Please answer one of {', '.join(labeled_alternatives)}")
 
-    return labeled_alternatives[answer_label]
+    # Changes the wording according to the number of choices
+    while True:
+        plural_s = "" if num_choices == 1 else f"s (choose {num_choices})"
+        answer = input(f"\nChoice{plural_s}? ")
+        answers = set(answer.replace(",", " ").split())
+
+        # Handles invalid answers
+        # Assigns user input to answer_label
+        if len(answers) != num_choices:
+            plural_s = "" if num_choices == 1 else "s, separated by comma"
+            print(f"Please answer {num_choices} alternative{plural_s}")
+            continue
+        if any(
+            (invalid := answer) not in labeled_alternatives
+            for answer in answers
+        ):
+            print(
+                f"{invalid!r} is not a valid choice. "
+                f"Please use {', '.join(labeled_alternatives)}"
+            )
+            continue
+         
+
+        return [labeled_alternatives[answer] for answer in answers]
 
 
 #sets correct_answer as the answer in the question dictionary in the toml table
 #calls get_answer() and compare user answer to correct_answer
 #print outcome and return 1 for correct and 0 for incorrect for score keeping
 def ask_question(question):
-    correct_answer = question["answer"]
-    alternatives = [question["answer"]] + question["alternatives"]
+    correct_answers = question["answers"]
+    alternatives = question["answers"] + question["alternatives"]
     #functionality to display alternatives in random order with each iteration
     ordered_alternatives = random.sample(alternatives, k=len(alternatives))
 
-    answer = get_answer(question["question"], ordered_alternatives)
-    if answer == correct_answer:
+    answers = get_answers(
+        question = question["question"], 
+        alternatives = ordered_alternatives,
+        num_choices = len(correct_answers),
+    )
+    if set(answers) == set(correct_answers):
         print("⭐ Correct! ⭐")
         return 1
     else:
-        print(f"The answer is {correct_answer!r}, not {answer!r}")
+        is_or_are = " is" if len(correct_answers) == 1 else "s are"
+        print("\n- ".join([f"No, the answer{is_or_are}: "] + correct_answers))
         return 0
 
 
@@ -89,4 +112,5 @@ if __name__ == "__main__":
 
 #Next-step
 #Step 5: Expand Your Quiz Functionality
-#https://realpython.com/python-quiz-application/#step-5-expand-your-quiz-functionality
+    # Add Hints to Help the User
+#https://realpython.com/python-quiz-application/#add-hints-to-help-the-user
